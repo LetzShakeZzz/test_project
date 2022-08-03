ab7ed9021443492b3d88f47402ae8508ac754642
LetzShake <qyz@mail.ru>
2022-08-03 03:12:05 +0300

    test_temp

--- temp.py
+++ temp.py
@@ -1,52 +1,36 @@
 from functools import reduce
 from math import log
 
-FILE_NAME = 'files.txt'
-RATES = {'B': 0, 'KB': 1, 'MB': 2, 'GB': 3}
-reverse_rates = {y: x for x, y in RATES.items()}
+rates = {'B': 0, 'KB': 1, 'MB': 2, 'GB': 3}
+reversed_rates = {y: x for x, y in rates.items()}
 
 
 def convert(position):
-    name, size, unit = position
-    return int(size) * 1024 ** RATES[unit]
+    full_name, size, unit = position
+    return int(size) * 1024 ** rates[unit]
 
 
-def calc_size(categories):
-    total_bytes = reduce(lambda x, y: x + y, map(lambda x: convert(x), categories), 0)
-    unit = reverse_rates[int(log(total_bytes, 1024))]
-    total = round(total_bytes / 1024 ** int(log(total_bytes, 1024)))
+def calc_size(cats):
+    total = reduce(lambda x, y: x + y, map(lambda x: convert(x), cats), 0)
+    unit = reversed_rates[int(log(total, 1024))]
+    total = round(total / 1024 ** int(log(total, 1024)))
     return (total, unit)
 
 
-def parse_input(fh):
-    parsed_files = {}
-    lines = fh.readlines()
-    for line in lines:
+with open('files.txt') as file:
+    files = {}
+
+    for line in file.readlines():
         full_name, size, unit = line.split()
         base_name, ext = full_name.split('.')
+        if ext not in files:
+            files[ext] = []
+        files[ext].append([full_name, size, unit])
 
-        if ext not in parsed_files:
-            parsed_files[ext] = []
-
-        parsed_files[ext].append([full_name, size, unit])
-
-    return parsed_files
-
-
-def print_stats(parsed_files):
-    for ext in sorted(parsed_files.keys()):
-        cat = parsed_files[ext]
-        print(*map(lambda x: x[0], sorted(cat)), sep='\n')
+    for ext in sorted(files.keys()):
+        cat = files[ext]
+        print(*map(lambda x: '.'.join(x[:1]), sorted(cat)), sep='\n')
         print('----------')
         total, unit = calc_size(cat)
         print(f'Summary: {total} {unit}')
         print()
-
-
-def main():
-    parsed_files = parse_input(open(FILE_NAME))
-    print_stats(parsed_files)
-
-
-if __name__ == "__main__":
-    main()
