6204123ce6a051457a6473fe77381845d7e0c6d4
LetzShake <qyz@mail.ru>
2022-08-10 02:45:20 +0300

    ver0.3

--- temp.py
+++ temp.py
@@ -1,42 +1,23 @@
-from functools import reduce
-from math import log
+import time
 
-def convert(position):
-    rates = {'B': 0, 'KB': 1, 'MB': 2, 'GB': 3}
-    name, ext, size, unit = position
-    return int(size) * 1024 ** rates[unit]
 
-def calc_size(cats):
-    rates = {0: 'B', 1: 'KB', 2: 'MB', 3: 'GB'}
-    total = reduce(lambda x, y: x + y, map(lambda x: convert(x), cats), 0)
-    unit = rates[int(log(total, 1024))]
-    total = round(total / 1024 ** int(log(total, 1024)))
-    return (total, unit)
+def for_and_append():  # с использованием цикла for и метода append()
+    iterations = 10_000_000
+    result = []
+    for i in range(iterations):
+        result.append(i + 1)
+    return result
 
-def main(input_file):
-    with open(input_file) as file:
-        file_lines = file.readlines()
 
-        extensions = set()
-        temp_files = []
-        for line in file_lines:
-            name, size, unit = line.split()
-            name, ext = name.split('.')
-            extensions.add(ext)
-            temp_files.append([name, ext, size, unit])
+def list_comprehension():  # с использованием списочного выражения
+    iterations = 10_000_000
+    return [i + 1 for i in range(iterations)]
 
-        extensions = {ext: num for num, ext in enumerate(extensions)}
 
-        sorted_files = [[] for _ in range(len(extensions))]
-        for position in temp_files:
-            ext = position[1]
-            sorted_files[extensions[ext]].append(position)
+t = time.perf_counter()
+for_and_append()
+print(f"for_and_append     = {time.perf_counter() - t} с.")
 
-        for cat in sorted(sorted_files, key=lambda x: x[0][1]):
-            print(*map(lambda x: '.'.join(x[:2]), sorted(cat)), sep='\n')
-            print('----------')
-            total, unit = calc_size(cat)
-            print(f'Summary: {total} {unit}')
-            print()
-
-main('files.txt')
+t = time.perf_counter()
+list_comprehension()
+print(f"list_comprehension = {time.perf_counter() - t} с.")
