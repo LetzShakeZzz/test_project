3af719c9dcb850d9bf782c35e648fe3f36cf59e1
LetzShake <qyz@mail.ru>
2022-09-05 00:04:37 +0300

    ver0.6

--- temp.py
+++ temp.py
@@ -1,5 +1,15 @@
 from zipfile import ZipFile
+from math import log
 
-with ZipFile('workbook.zip') as file:
-    info = file.namelist()
-    print(len(info))
+def calc_size(size):
+    total = round(size / 1024 ** int(log(size, 1024)))
+    unit = ('B', 'KB', 'MB', 'GB')[int(log(size, 1024))]
+    return f' {total} {unit}'
+
+with ZipFile('desktop.zip') as zip_file:
+    for entry in zip_file.infolist():
+        name = entry.filename.rstrip('/')
+        spaces = len(name.split('/')) - 1
+        size = calc_size(entry.file_size) if entry.file_size else ''
+        print(f"{'  ' * spaces}{name.split('/')[-1]}{size}")
+
