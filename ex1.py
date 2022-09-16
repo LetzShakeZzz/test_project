baf17a15c280a56e8657e30a22f9e01b2709744f
LetzShake <qyz@mail.ru>
2022-09-16 18:56:12 +0300

    ver0.9

--- ex1.py
+++ ex1.py
@@ -1,14 +1,3 @@
-from zipfile import ZipFile
-from math import log
+from collections import OrderedDict
 
-def calc_size(size):
-    total = round(size / 1024 ** int(log(size, 1024)))
-    unit = ('B', 'KB', 'MB', 'GB')[int(log(size, 1024))]
-    return f' {total} {unit}'
-
-with ZipFile('desktop.zip') as zip_file:
-    for entry in zip_file.infolist():
-        name = entry.filename.rstrip('/')
-        spaces = len(name.split('/')) - 1
-        size = calc_size(entry.file_size) if entry.file_size else ''
-        print(f"{'  ' * spaces}{name.split('/')[-1]}{size}")
+print(min([('Carol', 16), ('Brian', 40), ('Anabel', 17), ('Dustin', 29)]))
--- temp.py
+++ temp.py
@@ -1,9 +1,17 @@
 from collections import OrderedDict
 
 def custom_sort(ordered_dict, by_values=False):
-    global data
-    if by_values:
-        order = {letter: num for num, letter in enumerate(data.keys())}
-        data = OrderedDict(sorted(ordered_dict.items(), key=lambda x: (x[1], order[x[0]])))
-    else:
-        data = OrderedDict(sorted(ordered_dict.items()))
+    order = {letter: num for num, letter in enumerate(ordered_dict.keys())}
+    list_copy = list(ordered_dict.items())
+    ordered_dict.clear()
+    for _ in range(len(list_copy)):
+        min_item = min(list_copy, key=lambda x: (x, (x[1], order[x[0]]))[by_values])
+        key, value = list_copy.pop(list_copy.index(min_item))
+        ordered_dict[key] = value
+
+data1 = OrderedDict(e=11, b=22, a=99, g=33, c=33, d=33, h=99, f=77, i=88, k=44)
+custom_sort(data1, by_values=True)
+
+print(*data1.items())
+
+
